12902b7af2a92c57b9b4e91f1a8e422e3ffc3746
AyushThakali <thakaliaayush@gmail.com>
2026-04-07 22:24:43 +0545

    storeporovider updatd

--- lib/StoreProvider.tsx
+++ lib/StoreProvider.tsx
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import { AppStore, makeStore } from "./store";
 
 function StoreProvider({ children }: { children: React.ReactNode }) {
-  const storeRef = useRef<AppStore>();
+  const storeRef = useRef<AppStore>(undefined);
   if (!storeRef.current) {
     storeRef.current = makeStore();
   }
